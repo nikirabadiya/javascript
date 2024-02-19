bb55c43410115cd5400f055fd68144c695734177
Nikita <nikita@nes.tech>
2024-02-19 18:47:14 +0530

    Coversion of datatypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all js code as newer version
+
+let score = "33a";
+
+// console.log(typeof score);
+
+let valueInNumber = Number(score);
+
+// console.log(valueInNumber, typeof valueInNumber);
+
+let isLoggedIn = "hitesh";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+// 1 => true, 0 => false
+// 'test' => true, '' => false
+
+let someNumber = 33;
+
+let stringNumber = Boolean(someNumber);
+console.log(stringNumber);
