5951ecf9e98e9ec9667942f611220537bc39006e
Nikita <nikita@nes.tech>
2024-02-20 15:52:29 +0530

    Added comparison

--- 01_basics/dataTypes.js
+++ 01_basics/dataTypes.js
@@ -18,4 +18,21 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 let someNumber = 33;
 
 let stringNumber = Boolean(someNumber);
-console.log(stringNumber);
+// console.log(stringNumber);
+
+/************************ Operations ********************/
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+let str1 = "test";
+let str2 = " another";
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log(1 + "2");
+// console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
