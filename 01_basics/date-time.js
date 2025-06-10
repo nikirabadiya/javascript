9782d4cfcdfd1a21f378136fa55fd0f9f2b31758
Nikita Rabadiya <nikitarabadiya270@gmail.com>
2025-06-10 17:47:11 +0530

    Added proper notes for the first section

--- 01_basics/date-time.js
+++ 01_basics/date-time.js
@@ -1,30 +1,31 @@
 // Dates
 
 let myDate = new Date();
-// console.log(myDate.toString());
-// console.log(myDate.toDateString());
-// console.log(myDate.toTimeString());
-// console.log(myDate.toLocaleDateString());
-// console.log(myDate.toLocaleTimeString());
-// console.log(myDate.toLocaleString());
-// console.log(myDate.toISOString());
-// console.log(typeof myDate);
+// console.log(myDate); // 2025-06-10T12:00:09.872Z
+// console.log(myDate.toString()); // Tue Jun 10 2025 17:30:09 GMT+0530 (India Standard Time)
+// console.log(myDate.toDateString()); // Tue Jun 10 2025
+// console.log(myDate.toTimeString()); // 17:30:09 GMT+0530 (India Standard Time)
+// console.log(myDate.toLocaleDateString()); // 6/10/2025
+// console.log(myDate.toLocaleTimeString()); // 5:30:09 PM
+// console.log(myDate.toLocaleString()); // 6/10/2025, 5:30:09 PM
+// console.log(myDate.toISOString()); // 2025-06-10T12:00:09.872Z
+// console.log(typeof myDate); // object
 
-// let myCreatedDate = new Date(2023, 0, 23);
-// let myCreatedDate = new Date(2023, 0, 23, 5, 3);
-// let myCreatedDate = new Date("2023-01-14");
-let myCreatedDate = new Date("01-14-2023");
+// let myCreatedDate = new Date(2023, 0, 23); // 1/23/2023, 12:00:00 AM
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3); // 1/23/2023, 5:03:00 AM
+// let myCreatedDate = new Date("2023-01-14"); // 1/14/2023, 5:30:00 AM
+let myCreatedDate = new Date("01-14-2023"); // 1/14/2023, 12:00:00 AM
 // console.log(myCreatedDate.toLocaleString());
 
 let myTimeStamp = Date.now();
-// console.log(myTimeStamp);
-// console.log(myCreatedDate.getTime());
-// console.log(Math.floor(Date.now() / 1000));
+// console.log(myTimeStamp); // 1749557001287
+// console.log(myCreatedDate.getTime()); // 1673634600000
+// console.log(Math.floor(Date.now() / 1000)); // 1749557001
 
 let newDate = new Date();
-// console.log(newDate.getDate());
-// console.log(newDate.getMonth() + 1);
-// console.log(newDate.getDay());
+// console.log(newDate.getDate()); // 10
+// console.log(newDate.getMonth() + 1); // 6
+// console.log(newDate.getDay()); // 2
 
 newDate.toLocaleString("default", {
 	weekday: "long",
--- 01_basics/nums-maths.js
+++ 01_basics/nums-maths.js
@@ -1,30 +1,37 @@
 const score = 400;
 // console.log(score);
 
+// Alternate way to define the number
 const balance = new Number(100);
-// console.log(balance);
+// console.log(balance); // [Number: 100]
 
-// console.log(balance.toString().length);
-// console.log(balance.toFixed(2));  // makes the value beyond dot to fixed number
+// console.log(balance.toString().length); // 3
+// console.log(balance.toFixed(2)); // 100.00  // makes the value to fixed to the desired number beyond dot
 
 const otherNum = 23.8942;
-// console.log(otherNum.toPrecision(3));  // Makes the value round of to given number without counting the number after the dot, if it would 123.8 then it will be 124 as it is 3 digit number plus round off value of the given number
+// console.log(otherNum.toPrecision(3)); // 23.9  // Makes the value round of to given number without counting the number after the dot so the total number of the character will be the given number irrespectively it having the dot or not, if it would 123.8 then it will be 124 as it is 3 digit number but it will round off the value of the given number
 
 const theNum = 1000000;
-// console.log(theNum.toLocaleString("en-IN")); // makes the value understandable
+// console.log(theNum.toLocaleString()); // 1,000,000 // makes the value understandable
+// console.log(theNum.toLocaleString("en-IN")); // 10,00,000 // it makes the value understandable as per india
 
-/********************** Maths **********************/
+/********************************* MATHS *********************************/
 // console.log(Math);
-// console.log(Math.abs(-4)); // Changes the value into positive only
-// console.log(Math.round(4.3)); // Changes the value into integer only and round of
-// console.log(Math.ceil(4.3)); // Changes the value into integer only and top value of the given value, i.e - 4.1 or 4.9 hoga to 5 hi lega,
-// console.log(Math.floor(4.3)); // Changes the value into integer only and lower value of the given value, i.e - 4.1 or 4.9 hoga to 4 hi lega,
-// console.log(Math.min(4, 6, 9, 2));
-// console.log(Math.max(4, 6, 9, 2));
+
+// console.log(Math.abs(-4)); // 4 // Changes the value into positive(absolute) only whether it is postive or negetive, it will not convert the ppsitive value to negative, it only makes the nagetive value to positive value
+
+// console.log(Math.round(4.3)); // 4 // Changes the value into integer only and round of
+// console.log(Math.round(4.7)); // 5
+
+// console.log(Math.ceil(4.3)); // 5 // Changes the value into integer only and top value of the given value, i.e - 4.1 or 4.9 hoga to 5 hi lega
+// console.log(Math.floor(4.3)); // 4 // Changes the value into integer only and lower value of the given value, i.e - 4.1 or 4.9 hoga to 4 hi lega
+
+// console.log(Math.min(4, 6, 9, 2)); // 2
+// console.log(Math.max(4, 6, 9, 2)); // 9
 
 // console.log(Math.random()); // will give you value always between 0 to 1 but randomly
 
-// console.log(Math.random() * 10 + 1); // to get a value between 1 to 10
+// console.log((Math.random() * 10) + 1); // to get a value between 1 to 10
 // sometimes value can be there like 0.04 and even if we have added *10 it will still be 00.4 and will give us value as 0, so to avoid 0 as min value we have added 1
 
 const min = 10;
@@ -36,11 +43,11 @@ console.log(Math.floor(Math.random() * (max - min + 1)) + min);
 /*
 We have to create a random value between 10 to 20, and in console.log(Math.random() * 10 + 1) this formula we wanted to create a random value between 1 to 9 so we multiplied it by 10
 
-So first we need proper range between we want to create a number, which we can do by max-min, so it will be 10 same as above, so formula will be like Math.random() * (max - min + 1) as 1 is to avoid to generate min value 
+So first we need proper range between we want to create a number, which we can do by max-min, so it will be 10 same as above, so formula will be like Math.random() * (max - min + 1) as 1 is to avoid 0 
 
 Now we want only lower value, not round value so will add floor method to cut it only one integer number so formula will be Math.floor(Math.random() * (max - min + 1))
 
 But this will give us value between 1 to 10 as we understood the formula, so because we want the value between min and max numbers, we need to add min number to the formula so that the number starts creating from the min value - so final formula will be Math.floor(Math.random() * (max - min + 1)) + min
 
-We can check all above steps in clg - step by step to understand what is going on behind the screen
+We can check all above steps in console.log() - step by step to understand what is going on behind the screen
 */
