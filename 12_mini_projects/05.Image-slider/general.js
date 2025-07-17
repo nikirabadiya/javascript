7b0ce0f779927f8cae514c9cccaf2c4215fe2c04
Nikita Rabadiya <nikitarabadiya270@gmail.com>
2025-07-17 15:47:12 +0530

    Added some changes to the image slider and added one new functionality

--- 12_mini_projects/05.Image-slider/general.js
+++ 12_mini_projects/05.Image-slider/general.js
@@ -1,24 +1,36 @@
+const slider = document.querySelector(".slider");
 const slides = document.querySelectorAll(".slides .slide");
+const currentSlide = document.querySelector(".current");
+const totalSlides = document.querySelector(".total");
 let slideIndex = 0;
 let intervalId = null;
 
 function intializeSlider() {
 	if (slides.length > 0) {
-		slides[slideIndex].classList.add("display-slide");
-		// intervalId = setInterval(nextSlide, 10000); // for autoplay
+		showSlide(slideIndex);
+		totalSlides.textContent = slides.length;
+		intervalId = setInterval(nextSlide, 5000); // for autoplay
 	}
 }
+
+slider.addEventListener("mouseover", () => clearInterval(intervalId));
+slider.addEventListener("mouseleave", () => {
+	intervalId = setInterval(nextSlide, 5000);
+});
 
 function showSlide(index) {
 	if (index >= slides.length) {
 		slideIndex = 0;
 	} else if (index < 0) {
 		slideIndex = slides.length - 1;
+	} else {
+		slideIndex = index;
 	}
 	slides.forEach((slide) => {
 		slide.classList.remove("display-slide");
 	});
-	slides[index].classList.add("display-slide");
+	currentSlide.textContent = slideIndex + 1;
+	slides[slideIndex].classList.add("display-slide");
 }
 
 function prevSlide() {
@@ -34,3 +46,8 @@ function nextSlide() {
 
 // intializeSlider();
 document.addEventListener("DOMContentLoaded", intializeSlider);
+
+document.addEventListener("keydown", (e) => {
+	if (e.key === "ArrowRight") nextSlide();
+	if (e.key === "ArrowLeft") prevSlide();
+});
